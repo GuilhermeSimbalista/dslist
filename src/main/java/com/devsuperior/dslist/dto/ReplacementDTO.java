92ea4d40b26bc8afed71fe32dfdfcdc117b93b25
Guilherme <guilherme.simbalista@yahoo.com.br>
2023-07-13 22:14:47 -0300

    Move games on list

---
+++ src/main/java/com/devsuperior/dslist/dto/ReplacementDTO.java
@@ -0,0 +1,23 @@
+package com.devsuperior.dslist.dto;
+
+public class ReplacementDTO {
+
+    private Integer sourceindex;
+    private Integer destinationIndex;
+
+    public Integer getSourceindex() {
+        return sourceindex;
+    }
+
+    public void setSourceindex(Integer sourceindex) {
+        this.sourceindex = sourceindex;
+    }
+
+    public Integer getDestinationIndex() {
+        return destinationIndex;
+    }
+
+    public void setDestinationIndex(Integer destinationIndex) {
+        this.destinationIndex = destinationIndex;
+    }
+}
